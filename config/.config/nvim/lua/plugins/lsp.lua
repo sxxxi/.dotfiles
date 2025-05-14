ec2a9422dfa22c4e4e764b6d47c328a3ddd8800d
Seiji Akakabe <seiji@macmac.local>
2025-05-15 08:15:05 +0900

    new formatting plugin

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -32,21 +32,110 @@ return {
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
     },
-    init = function()
-      -- Reserve a space in the gutter
-      -- This will avoid an annoying layout shift in the screen
-      vim.opt.signcolumn = 'yes'
-    end,
     config = function()
-      local lsp_defaults = require('lspconfig').util.default_config
+      -- local lsp_defaults = require('lspconfig').util.default_config
+
+      -- Global server config
+      vim.lsp.config("*", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities()
+      })
+
+      -- Intelephense
+      vim.lsp.config("intelephense", {
+        root_markers = {
+          'wp-config.php',
+          'wp-includes',
+          'composer.json',
+          'artisan',
+          '.git',
+        },
+        settings = {
+          ["intelephense"] = {
+            format = {
+              enable = true,
+              braces = "k&r",
+            }
+          }
+        }
+      })
+
+      -- Biome
+      vim.lsp.config("biome", {
+        cmd = { "biome", "lsp-proxy" },
+        settings = {
+          ["biome"] = {
+            formatter = {
+              indentstyle = "space",
+            },
+          }
+        }
+      })
+
+
+      --[[
+          require('lspconfig').lua_ls.setup({
+            settings = {
+              Lua = {
+                telemetry = {
+                  enable = false
+                },
+              },
+            },
+            on_init = function(client)
+              local join = vim.fs.joinpath
+              local path = client.workspace_folders[1].name
+
+              -- Don't do anything if there is project local config
+              if vim.uv.fs_stat(join(path, '.luarc.json'))
+                  or vim.uv.fs_stat(join(path, '.luarc.jsonc'))
+              then
+                return
+              end
+
+              local nvim_settings = {
+                runtime = {
+                  -- Tell the language server which version of Lua you're using
+                  version = 'LuaJIT',
+                },
+                diagnostics = {
+                  -- Get the language server to recognize the `vim` global
+                  globals = { 'vim' }
+                },
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    -- Make the server aware of Neovim runtime files
+                    vim.env.VIMRUNTIME,
+                    vim.fn.stdpath('config'),
+                  },
+                },
+              }
+
+              client.config.settings.Lua = vim.tbl_deep_extend(
+                'force',
+                client.config.settings.Lua,
+                nvim_settings
+              )
+            end,
+          })
+        ]]
+
+
+
+
+
+
+
 
       -- Add cmp_nvim_lsp capabilities settings to lspconfig
       -- This should be executed before you configure any language server
+      --[[
       lsp_defaults.capabilities = vim.tbl_deep_extend(
         'force',
         lsp_defaults.capabilities,
         require('cmp_nvim_lsp').default_capabilities()
       )
+      ]]
 
       -- LspAttach is where you enable features that only work
       -- if there is a language server active in the file
@@ -67,11 +156,49 @@ return {
           vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
         end,
       })
+    end,
 
-      -- These are just examples. Replace them with the language
-      -- servers you have installed in your system
-      require('lspconfig').gleam.setup({})
-      require('lspconfig').ocamllsp.setup({})
-    end
+    init = function()
+      -- Reserve a space in the gutter
+      -- This will avoid an annoying layout shift in the screen
+      vim.opt.signcolumn = 'yes'
+    end,
   },
+  {
+    "mhartington/formatter.nvim",
+    config = function()
+      -- Utilities for creating configurations
+      local util = require "formatter.util"
+
+      -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+      require("formatter").setup {
+        -- Enable or disable logging
+        logging = true,
+        -- Set the log level
+        log_level = vim.log.levels.WARN,
+        -- All formatter configurations are opt-in
+        filetype = {
+          javascript = {
+            require("formatter.filetypes.javascript"),
+            function()
+              return {
+                exe = "biome",
+                args = {
+                  "format",
+                  "--indent-style=space",
+                  "--indent-width=2",
+                  "--jsx-quote-style=double",
+                  "--write",
+                }
+              }
+            end
+          },
+
+          ["*"] = {
+            require("formatter.filetypes.any").remove_trailing_whitespace,
+          }
+        }
+      }
+    end
+  }
 }
